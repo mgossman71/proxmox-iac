64b2578e2df17d42da73f64132feaef2cf24b66e
Mark Gossman <mark.gossman71@gmail.com>
2026-03-13 13:53:26 -0500

    update

--- stacks/lab/main.tf
+++ stacks/lab/main.tf
@@ -223,18 +223,18 @@ locals {
 
   vms = {
     # Same-node clone — template (501) and new VM both on pve-t0
-    "vm-test-1" = merge(local.vm_defaults, {
-      vmid               = 600
-      node_name          = "pve-t0"
-      name               = "vm-test-1"
-      ipv4_address       = "dhcp"
-      ipv4_gateway       = null
-      clone_vm_id        = 501
-      clone_node_name    = null
-      datastore_id       = "local-lvm"
-      cloud_init_enabled = true
-      tags               = ["iac", "lab", "vm", "clone"]
-    })
+#   "vm-test-1" = merge(local.vm_defaults, {
+#     vmid               = 600
+#     node_name          = "pve-t0"
+#     name               = "vm-test-1"
+#     ipv4_address       = "dhcp"
+#     ipv4_gateway       = null
+#     clone_vm_id        = 501
+#     clone_node_name    = null
+#     datastore_id       = "local-lvm"
+#     cloud_init_enabled = true
+#     tags               = ["iac", "lab", "vm", "clone"]
+#   })
 
 #   # Cross-node clone — template (501) on pve-t0, deploy to pve-t1
 #   # datastore_id must be shared storage so pve-t1 can receive the cloned disk
